2f8c007a70a9bd7b32607d1d808e01ccbeaa051f
Peter <piowiedlocha@gmail.com>
2026-04-09 22:00:17 +0200

    prod swagger fix, nginx fix

--- backend/src/main/java/com/poetrystream/backend/BackendApplication.java
+++ backend/src/main/java/com/poetrystream/backend/BackendApplication.java
@@ -2,6 +2,8 @@ package com.poetrystream.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +11,10 @@ public class BackendApplication {
     public static void main(String[] args) {
         var context = SpringApplication.run(BackendApplication.class, args);
         System.out.println("Beans w kontekście: " + context.getBeanDefinitionCount());
+    }
+
+    @Bean
+    ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
     }
 }
